56a88db9981cd20930033d6765c434df3d81f538
Vijay Akula <vijay.akula@ustechsolutions.com>
2024-06-30 19:05:09 -0400

    initial commit

---
+++ src/main/java/com/uw/uaa/config/JwtDecoderConfig.java
@@ -0,0 +1,20 @@
+package com.uw.uaa.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class JwtDecoderConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
